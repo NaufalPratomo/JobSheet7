c75dcce65cf6c3489b7861abe07e73e2d3d2110c
Naufal Pratomo <naufalpratomo8@gmail.com>
2023-10-16 13:38:24 +0700

    manambahkan variable rata-rata

--- ForKelipatan19.java
+++ ForKelipatan19.java
@@ -5,6 +5,7 @@ public class ForKelipatan19 {
         Scanner scan = new Scanner(System.in);
 
         int kelipatan;
+        double rata2;
         int total = 0, counter = 0;
 
         System.out.print("Masukkan bilangan kelipatan (1 - 9): ");
@@ -17,7 +18,10 @@ public class ForKelipatan19 {
             }
         }
         
-        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+        rata2 = (double) total / counter;
+
+        System.out.printf("Banyaknya kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
+        System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %.2f\n",kelipatan, rata2);
     }
 }
